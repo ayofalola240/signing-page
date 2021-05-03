16ebde2275c64cd41811cd4899c3477d07def227
Falola Ibrahim A <ayofalola240@gmail.com>
2021-05-03 21:00:50 +0100

    consume api for the login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { React } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Signin from './components/signin/Signin'
 import Register from './components/register/Register'
 import Header from './components/header/Header';
@@ -7,9 +9,11 @@ function App() {
   return (
     <Router>
       <Header />
+      <ToastContainer autoClose={2000} />
       <main className='main'>
         <Route path='/login' component={Signin} />
         <Route path='/register' component={Register} />
+        <Route path='/' component={Signin} exact />
       </main>
     </Router>
 
--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -1,15 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import axios from 'axios'
 import './Signin.css'
 
 const Signin = () => {
-    const [email, setEmail] = useState('email')
-    const [password, setPassword] = useState('password')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const login = async () => {
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const { data } = await axios.post('https://medicaly-internship-api.herokuapp.com/login', { email, password }, config)
+
+                toast.success(`Welcome back ${data.data.email}`);
+
+            } catch (err) {
+                console.log(err)
+                toast.error('Error! try again.');
+
+            }
+
+        }
+        login()
+    }
     return (
         <div className='logContainer'>
             <div className='row'>
-
                 <div className='column-form'>
-                    <form id="form" className='sign-up-form' >
+                    <form id="form" className='sign-up-form' onSubmit={submitHandler}>
                         <h1 className='form-title'>Sign Up</h1>
                         <div className="input-field">
                             <label htmlFor="email" >Email/Username</label>
@@ -25,12 +50,12 @@ const Signin = () => {
                             </input>
                         </div>
                         <div className="form__actions">
-                            <label for="checkboxInput" className="remeber_me">
+                            <label htmlFor="checkboxInput" className="remeber_me">
                                 <input type="checkbox" id="checkboxInput"></input>
                                 <span className="checkmark"></span>
                                 <span>Remeber Me</span>
                             </label>
-                            <div className="forgot_password">Forgot Password?</div>
+                            <div className="forgot_password"><span>Forgot Password?</span></div>
                         </div>
                         <button className='submit-button' type='submit'>Login</button>
                     </form>
@@ -38,7 +63,7 @@ const Signin = () => {
 
                 </div>
                 <div className='column-image'>
-                    <img src={process.env.PUBLIC_URL + 'image/login.svg'} className='image' />
+                    <img src={process.env.PUBLIC_URL + 'image/login.svg'} className='image' alt='illustration' />
                 </div>
             </div>
         </div>
